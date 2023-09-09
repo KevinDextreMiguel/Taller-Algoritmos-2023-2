cbb98a442e04ba12f030bc2e1a6614c181844dbe
KevinDextreMiguel <123123234+KevinDextreMiguel@users.noreply.github.com>
2023-09-09 15:30:19 -0500

    Update Source.cpp

--- Semana 4/Ejercicio 3/Source.cpp
+++ Semana 4/Ejercicio 3/Source.cpp
@@ -1,29 +1,9 @@
-//#include"ArrCaracter.h"
 #include"ArrAvion.h"
 int main() {
 	Console::SetWindowSize(ANCHO_CONSOLA, ALTO_CONSOLA);
 	Console::CursorVisible = false;
 	srand(time(0));
 	int n = rand() % 3 + 3;
-	ArrAvion* obj = new ArrAvion(n);
-	//ArrCaracter* obj = new ArrCaracter();
-	//
-	//while (true)
-	//{
-	//	char tecla;
-	//	if (_kbhit())
-	//	{
-	//		tecla = getch();
-	//		if (tecla == '1')
-	//			obj->agregar(new Caracter(1,0));
-	//		else if (tecla == '2')
-	//			obj->agregar(new Caracter(0, 1));
-	//		else if (tecla == '3')
-	//			obj->agregar(new Caracter(1, 1));
-	//	}
-	//	obj->mostrar_todo();
-	//	_sleep(100);
-	//}
 
 	obj->generar_Aviones();
 
